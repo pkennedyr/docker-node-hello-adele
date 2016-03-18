4c2d57f787360afa55bead88c0b0099925305b45
pkennedyr <pkennedyr@users.noreply.github.com>
2016-03-18 12:45:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || DEFAULT_PORT;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello Adele its Ryan Kennedy\n');
+  res.send('Hello Docker Community\n');
 });
 
 app.listen(PORT)
