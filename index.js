7d728e881d0250c34e6ffaa89004bb3a1a5aff9c
Ryan Kennedy <ryankennedy@C02NH0DFG3QDs-MacBook-Pro.local>
2016-06-14 15:38:06 -0700

    Changed text

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || DEFAULT_PORT;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello Docker builders!\n');
+  res.send('Hello Dockerconers!\n');
 });
 
 app.listen(PORT)
