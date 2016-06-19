3b43936b16cc2891c9757badbeb50586684a300f
Ryan Kennedy <ryankennedy@C02NH0DFG3QDs-MacBook-Pro.local>
2016-06-19 14:52:46 -0700

    Changed name

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || DEFAULT_PORT;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello Ryan!\n');
+  res.send('Hello Mo!\n');
 });
 
 app.listen(PORT)
